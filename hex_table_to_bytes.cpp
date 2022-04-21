9b8741cf88090a3a42b5797945053fd7fa7748cf
Tony Hua <tony7068@gmail.com>
2022-04-20 23:35:49 -0700

    Read hex table and output corresponding string

--- hex_table_to_bytes.cpp
+++ hex_table_to_bytes.cpp
@@ -12,14 +12,61 @@ int to_int(string str, int base);
 char to_char(int num);
 string to_string(int num, int base);
 
-
 int hex_to_int(char c);
 int binary_to_int(char c);
+
+int char_to_dec(string str);
+int hex_to_dec(string str);
+int bin_to_dec(string str);
+bool is_byte_empty(string str);
+
+// void find_formatted_byte(string line) {
+// 	int j = 0, k = 0, m = 0, n = 0;
+// 	int column_dec[4];
+
+// 	// cout << line << endl;
+
+// 	for (int i = 0; i < 4; ++i) {
+// 		if (is_byte_empty(line.substr(3 + j, 4))) {
+// 			j += 5;
+// 			k += 3;
+// 			m += 9;
+// 			n += 4;
+			
+// 			continue;
+// 		} else {
+// 			column_dec[0] = char_to_dec(line.substr(3 + j, 4));
+// 			column_dec[1] = hex_to_dec(line.substr(24 + k, 2));
+// 			column_dec[2] = bin_to_dec(line.substr(38 + m, 8));
+// 			column_dec[3] = stoi(line.substr(76 + n, 3));
+
+// 			if (column_dec[0] != column_dec[1] && column_dec[0] != column_dec[2] && column_dec[0] != column_dec[3]) {
+// 				cout << static_cast<char>(column_dec[0]); 
+// 			} else if (column_dec[1] != column_dec[0] && column_dec[1] != column_dec[2] && column_dec[1] != column_dec[3]) {
+// 				cout << static_cast<char>(column_dec[1]); 
+// 			} else if (column_dec[2] != column_dec[0] && column_dec[2] != column_dec[1] && column_dec[2] != column_dec[3]) {
+// 				cout << static_cast<char>(column_dec[2]); 
+// 			} else if (column_dec[3] != column_dec[0] && column_dec[3] != column_dec[1] && column_dec[3] != column_dec[2]) {
+// 				cout << static_cast<char>(column_dec[3]); 
+// 			} else {
+// 				cout << static_cast<char>(column_dec[0]); 
+// 			}
+// 		}
+
+// 		j += 5;
+// 		k += 3;
+// 		m += 9;
+// 		n += 4;
+// 	}
+// }
 
 int main(int argc, const char *argv[]) {
 	string line;
 	
-	ifstream input("hi_hex.txt");
+	// ifstream input("hi_hex.txt");
+
+	ifstream input(argv[1]);
+	ofstream output(argv[2]);
 	
 	getline(input, line);
 	if (line != "| Printable           | Hexadecimal | Binary                              | Decimal         |") {
@@ -32,67 +79,45 @@ int main(int argc, const char *argv[]) {
 	}
 
 	while (getline(input, line)) {
-		int j = 0, k = 0, m = 0, n = 0;
+		// find_formatted_byte(line);
 
-		cout << line << endl;
+		int j = 0, k = 0, m = 0, n = 0;
+		int column_dec[4];
+
+		// cout << line << endl;
 
 		for (int i = 0; i < 4; ++i) {
+			if (is_byte_empty(line.substr(3 + j, 4))) {
+				j += 5;
+				k += 3;
+				m += 9;
+				n += 4;
+				
+				continue;
+			} else {
+				column_dec[0] = char_to_dec(line.substr(3 + j, 4));
+				column_dec[1] = hex_to_dec(line.substr(24 + k, 2));
+				column_dec[2] = bin_to_dec(line.substr(38 + m, 8));
+				column_dec[3] = stoi(line.substr(76 + n, 3));
 
-			string s3 = line.substr(3 + j, 4);
-			char buf3[4];
-			buf3[0] = s3[0];
-			buf3[1] = s3[1];
-			buf3[2] = s3[2];
-			buf3[3] = s3[3];
-			if (buf3[0] == 'x') {
-				cout << hex_to_int(buf3[1]) * 16 + hex_to_int(buf3[2]) << endl;
-			} else if (buf3[0] == ' ' && buf3[1] == '\\') {
-				switch (buf3[2]) {
-				case '0':
-					cout << 0 << endl;
-				case 't':
-					cout << 9 << endl;
-				case 'n':
-					cout << 10 << endl;
+				if (column_dec[0] != column_dec[1] && column_dec[0] != column_dec[2] && column_dec[0] != column_dec[3]) {
+					output << static_cast<char>(column_dec[0]); 
+				} else if (column_dec[1] != column_dec[0] && column_dec[1] != column_dec[2] && column_dec[1] != column_dec[3]) {
+					output << static_cast<char>(column_dec[1]); 
+				} else if (column_dec[2] != column_dec[0] && column_dec[2] != column_dec[1] && column_dec[2] != column_dec[3]) {
+					output << static_cast<char>(column_dec[2]); 
+				} else if (column_dec[3] != column_dec[0] && column_dec[3] != column_dec[1] && column_dec[3] != column_dec[2]) {
+					output << static_cast<char>(column_dec[3]); 
+				} else {
+					output << static_cast<char>(column_dec[0]); 
 				}
-			} else if (buf3[0] == ' ' && buf3[1] == ' ') {
-				cout << int(buf3[2]) << endl;
-			} else if (buf3[0] == '\'' && buf3[1] == ' ' && buf3[2] == '\'') {
-				cout << 32 << endl;
 			}
-			// cout << line.substr(3 + j, 4) << endl; // printable
-
-			string s = line.substr(24 + k, 2);
-			char buf[2];
-			buf[0] = s[0];
-			buf[1] = s[1];
-			int num = hex_to_int(buf[0]) * 16 + hex_to_int(buf[1]);
-			cout << num << endl;
-			// cout << line.substr(24 + k, 2) << endl; // hexadecimal
-			
-			string s2 = line.substr(38 + m, 8);
-			char buf2[8];
-			buf2[0] = s2[0];
-			buf2[1] = s2[1];
-			buf2[2] = s2[2];
-			buf2[3] = s2[3];
-			buf2[4] = s2[4];
-			buf2[5] = s2[5];
-			buf2[6] = s2[6];
-			buf2[7] = s2[7];
-			int num2 = binary_to_int(buf2[0]) * 128 + binary_to_int(buf2[1]) * 64 + binary_to_int(buf2[2]) * 32 + binary_to_int(buf2[3]) * 16 + binary_to_int(buf2[4]) * 8 + binary_to_int(buf2[5]) * 4 + binary_to_int(buf2[6]) * 2 + binary_to_int(buf2[7]);
-			cout << num2 << endl;
-			// cout << line.substr(38 + m, 8) << endl; // binary
-
-			cout << stoi(line.substr(76 + n, 3)) << endl; // decimal
 
 			j += 5;
 			k += 3;
 			m += 9;
 			n += 4;
 		}
-
-		cout << endl;
 	}
 
 	return 0;
@@ -138,7 +163,6 @@ string to_string(int num, int base=10) {
     return result;
 }
 
-
 int hex_to_int(char c) {
   if (c >= '0' && c <= '9') {
     return c - '0';
@@ -153,4 +177,63 @@ int binary_to_int(char c) {
     return c - '0';
   }
   throw runtime_error("Char is not a binary char!");
+}
+
+int char_to_dec(string str) {
+	char buf[4];
+
+	for (int i = 0; i < str.length(); ++i) {
+		buf[i] = str[i];
+	}
+
+	if (buf[0] == 'x') {
+		return hex_to_int(buf[1]) * 16 + hex_to_int(buf[2]);
+	} else if (buf[0] == ' ' && buf[1] == '\\') {
+		switch (buf[2]) {
+		case '0':
+			return 0;
+		case 't':
+			return 9;
+		case 'n':
+			return 10;
+		}
+	} else if (buf[0] == ' ' && buf[1] == ' ') {
+		return int(buf[2]);
+	} else if (buf[0] == '\'' && buf[1] == ' ' && buf[2] == '\'') {
+		return 32;
+	}
+	throw runtime_error("Could not convert chararacter to a decimal!");
+}
+
+int hex_to_dec(string str) {
+	char buf[2];
+
+	buf[0] = str[0];
+	buf[1] = str[1];
+	
+	return hex_to_int(buf[0]) * 16 + hex_to_int(buf[1]);
+}
+
+int bin_to_dec(string str) {
+	char buf[8];
+
+	for (int i = 0; i < str.length(); ++i) {
+		buf[i] = str[i];
+	}
+
+	return binary_to_int(buf[0]) * 128 + binary_to_int(buf[1]) * 64 + binary_to_int(buf[2]) * 32 + binary_to_int(buf[3]) * 16 + binary_to_int(buf[4]) * 8 + binary_to_int(buf[5]) * 4 + binary_to_int(buf[6]) * 2 + binary_to_int(buf[7]);
+}
+
+bool is_byte_empty(string str) {
+	char buf[4];
+
+	for (int i = 0; i < str.length(); ++i) {
+		buf[i] = str[i];
+	}
+
+	if (buf[0] == ' ' && buf[1] == ' ' && buf[2] == ' ') {
+		return true;
+	} else {
+		return false;
+	}
 }
